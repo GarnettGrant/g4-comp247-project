5aeb29c040f0c93e45276385a648d07965a8f319
Victor Zorn <vzorn@my.centennialcollege.ca>
2024-03-17 16:43:05 -0400

    changed csv path and added docstring

--- Group4_Project_Phase1.py
+++ Group4_Project_Phase1.py
@@ -1,28 +1,50 @@
+"""
+COMP247-402
+Final Project: Phase 1
+
+File name:     Group4_Project_Phase1.py
+Student names: Damien Liscio
+               Cole Ramsey
+               Victor Zorn (301197146)
+               Garnett Grant
+Due date: Mar. 17, 2024
+"""
+
+# imports
 import pandas as pd
 import matplotlib.pyplot as plt
 
-KSI_data = pd.read_csv("/Users/damie/Downloads/KSI.csv")  #Load KSI dataset into data frame
+# Section 1: Data exploration
 
-#Basic column exploration
-column_names = KSI_data.columns    #Get column names
-column_types = KSI_data.dtypes   #Get column types
-column_missing_data = KSI_data.isnull().sum()  #Get sum of missing values for each column
-column_descriptions = KSI_data.describe()  #Get column descriptions
-for column in column_names:    #Loop through each column
-    if column in column_descriptions:    #Check if the column is in list of columns that have descriptions
-        print('Column Name:',column, 'Column Type:',column_types[column], 'Sum Of Column Missing Values:', column_missing_data[column], '\nColumn Description:\n',column_descriptions[column], '\n')    #Display column names, types and descriptions
+# load KSI dataset into data frame
+KSI_data = pd.read_csv('KSI.csv')
+
+# basic column exploration
+column_names = KSI_data.columns # get column names
+column_types = KSI_data.dtypes # get column types
+column_missing_data = KSI_data.isnull().sum() # get sum of missing values for each column
+column_descriptions = KSI_data.describe() # get column descriptions
+
+# loop through each column
+for column in column_names:
+    # check if column is in list of columns that have descriptions
+    if column in column_descriptions:
+        # display column names, types and descriptions
+        print('Column Name:', column, 'Column Type:', column_types[column],
+              'Sum Of Column Missing Values:', column_missing_data[column],
+              '\nColumn Description:\n', column_descriptions[column], '\n')
     else:
-        print('Column Name:',column, 'Column Type:',column_types[column], 'Sum Of Column Missing Values:', column_missing_data[column], '\nColumn Description Not Available\n')  #Display column names and types for those without descriptions
-        
-#Plot quantity of each injury type by year
-injury_type_counts = KSI_data.groupby(['YEAR', 'INJURY']).size().unstack(fill_value=0)  #Get quantities for injury counts by year
-plt.figure(figsize=(8,6))  #Set plot size
-injury_type_counts.plot(kind='bar', stacked=True)   #Plot data as bar graph
-plt.xlabel('Year')          #Set plot xlabel
-plt.ylabel('Number of Reports Per Injury Type')   #Set plot ylabel
-plt.title('Quantity of Injury Type by Year')   #set plot title
-plt.legend(title='Injury Type')   #Set plot legends title
-plt.show()   #Show the plot
+        # display column names and types for those without descriptions
+        print('Column Name:', column, 'Column Type:', column_types[column],
+              'Sum Of Column Missing Values:', column_missing_data[column],
+              '\nColumn Description Not Available\n')
 
-
-
+# plot quantity of each injury type by year
+injury_type_counts = KSI_data.groupby(['YEAR', 'INJURY']).size().unstack(fill_value=0) # get quantities for injury counts by year
+plt.figure(figsize=(8,6))
+injury_type_counts.plot(kind='bar', stacked=True) # plot data as bar graph
+plt.xlabel('Year')
+plt.ylabel('Number of Reports Per Injury Type')
+plt.title('Quantity of Injury Type by Year')
+plt.legend(title='Injury Type')
+plt.show()
